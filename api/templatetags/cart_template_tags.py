8cd505e753dfba70ec8fb2533294105ef35a4e1f
Your Name <hoainho@HoaiNho-Macbook.local>
2023-12-19 01:29:03 +0700

    init project

---
+++ api/templatetags/cart_template_tags.py
@@ -0,0 +1,18 @@
+from django import template
+from api.models import Order
+
+register = template.Library()
+
+
+@register.filter
+def cart_item_count(user):
+    if user.is_authenticated:
+        qs = Order.objects.filter(user=user, ordered=False)
+        if qs.exists():
+            return qs[0].items.count()
+    return 0
+
+@register.filter
+def toggle_navbar(status):
+    return not status
+
---
+++ resume-admin-django/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for resumeAdminDjango project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resume-admin-django.settings')
+
+application = get_wsgi_application()
